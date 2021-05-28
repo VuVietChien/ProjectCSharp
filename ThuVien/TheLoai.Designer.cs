11b194421f63c2cc94ea0ac57177b3f4d9104511
datnguyen2001 <nguyendatdz01@gmail.com>
2021-05-28 23:44:32 +0700

    datupdate

--- ThuVien/TheLoai.Designer.cs
+++ ThuVien/TheLoai.Designer.cs
@@ -28,12 +28,169 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.maTheLoaitextBox = new System.Windows.Forms.TextBox();
+            this.tenTheLoaitextBox = new System.Windows.Forms.TextBox();
+            this.TimkiemtextBox = new System.Windows.Forms.TextBox();
+            this.btnthem = new System.Windows.Forms.Button();
+            this.btnsua = new System.Windows.Forms.Button();
+            this.btnxoa = new System.Windows.Forms.Button();
+            this.btnlammoi = new System.Windows.Forms.Button();
+            this.btnback = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(138, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã thể loại:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(138, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Thên thể loại:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(138, 198);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tìm kiếm";
+            // 
+            // maTheLoaitextBox
+            // 
+            this.maTheLoaitextBox.Location = new System.Drawing.Point(279, 92);
+            this.maTheLoaitextBox.Name = "maTheLoaitextBox";
+            this.maTheLoaitextBox.Size = new System.Drawing.Size(100, 22);
+            this.maTheLoaitextBox.TabIndex = 3;
+            // 
+            // tenTheLoaitextBox
+            // 
+            this.tenTheLoaitextBox.Location = new System.Drawing.Point(279, 138);
+            this.tenTheLoaitextBox.Name = "tenTheLoaitextBox";
+            this.tenTheLoaitextBox.Size = new System.Drawing.Size(100, 22);
+            this.tenTheLoaitextBox.TabIndex = 4;
+            // 
+            // TimkiemtextBox
+            // 
+            this.TimkiemtextBox.Location = new System.Drawing.Point(279, 198);
+            this.TimkiemtextBox.Name = "TimkiemtextBox";
+            this.TimkiemtextBox.Size = new System.Drawing.Size(100, 22);
+            this.TimkiemtextBox.TabIndex = 5;
+            this.TimkiemtextBox.TextChanged += new System.EventHandler(this.TimkiemtextBox_TextChanged);
+            // 
+            // btnthem
+            // 
+            this.btnthem.Location = new System.Drawing.Point(502, 90);
+            this.btnthem.Name = "btnthem";
+            this.btnthem.Size = new System.Drawing.Size(75, 23);
+            this.btnthem.TabIndex = 6;
+            this.btnthem.Text = "Thêm";
+            this.btnthem.UseVisualStyleBackColor = true;
+            this.btnthem.Click += new System.EventHandler(this.btnthem_Click);
+            // 
+            // btnsua
+            // 
+            this.btnsua.Location = new System.Drawing.Point(502, 132);
+            this.btnsua.Name = "btnsua";
+            this.btnsua.Size = new System.Drawing.Size(75, 23);
+            this.btnsua.TabIndex = 7;
+            this.btnsua.Text = "Sửa";
+            this.btnsua.UseVisualStyleBackColor = true;
+            this.btnsua.Click += new System.EventHandler(this.btnsua_Click);
+            // 
+            // btnxoa
+            // 
+            this.btnxoa.Location = new System.Drawing.Point(502, 179);
+            this.btnxoa.Name = "btnxoa";
+            this.btnxoa.Size = new System.Drawing.Size(75, 23);
+            this.btnxoa.TabIndex = 8;
+            this.btnxoa.Text = "Xóa";
+            this.btnxoa.UseVisualStyleBackColor = true;
+            this.btnxoa.Click += new System.EventHandler(this.btnxoa_Click);
+            // 
+            // btnlammoi
+            // 
+            this.btnlammoi.Location = new System.Drawing.Point(502, 222);
+            this.btnlammoi.Name = "btnlammoi";
+            this.btnlammoi.Size = new System.Drawing.Size(75, 23);
+            this.btnlammoi.TabIndex = 9;
+            this.btnlammoi.Text = "Làm mới";
+            this.btnlammoi.UseVisualStyleBackColor = true;
+            this.btnlammoi.Click += new System.EventHandler(this.btnlammoi_Click);
+            // 
+            // btnback
+            // 
+            this.btnback.Location = new System.Drawing.Point(12, 12);
+            this.btnback.Name = "btnback";
+            this.btnback.Size = new System.Drawing.Size(75, 23);
+            this.btnback.TabIndex = 10;
+            this.btnback.Text = "button5";
+            this.btnback.UseVisualStyleBackColor = true;
+            this.btnback.Click += new System.EventHandler(this.btnback_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(56, 288);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(492, 150);
+            this.dataGridView1.TabIndex = 12;
+            this.dataGridView1.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseClick);
+            // 
+            // TheLoai
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnback);
+            this.Controls.Add(this.btnlammoi);
+            this.Controls.Add(this.btnxoa);
+            this.Controls.Add(this.btnsua);
+            this.Controls.Add(this.btnthem);
+            this.Controls.Add(this.TimkiemtextBox);
+            this.Controls.Add(this.tenTheLoaitextBox);
+            this.Controls.Add(this.maTheLoaitextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "TheLoai";
             this.Text = "TheLoai";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox maTheLoaitextBox;
+        private System.Windows.Forms.TextBox tenTheLoaitextBox;
+        private System.Windows.Forms.TextBox TimkiemtextBox;
+        private System.Windows.Forms.Button btnthem;
+        private System.Windows.Forms.Button btnsua;
+        private System.Windows.Forms.Button btnxoa;
+        private System.Windows.Forms.Button btnlammoi;
+        private System.Windows.Forms.Button btnback;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
